255ecc83855f245a8621ddc67fcbf00176ba755d
wsa <bensonwang2001@gmail.com>
2023-11-10 01:10:04 +0800

    chore: abstract file storage handler

--- app/persistence/file_storage/gcs.py
+++ app/persistence/file_storage/gcs.py
@@ -5,10 +5,12 @@ from uuid import UUID, uuid4
 from google.cloud import storage
 
 from app.base import mcs
+from app.persistence.file_storage import BaseFileHandler
 
 
-class GCSHandler(metaclass=mcs.Singleton):
+class GCSHandler(BaseFileHandler, metaclass=mcs.Singleton):
     def __init__(self):
+        super().__init__()
         self.client: storage.Client = None  # noqa
 
     def initialize(self):
@@ -23,12 +25,12 @@ class GCSHandler(metaclass=mcs.Singleton):
         bucket = self.client.get_bucket(bucket_name)
         return storage.blob.Blob(bucket=bucket, name=filename)
 
-    async def sign_url(self, method, bucket_name: str, filename, expire_time: int = 3600):
+    async def sign_url(self, bucket_name: str, filename: str, method: str = 'GET', expire_time: int = 3600):
         blob = await self.get_blob(bucket_name, filename)
         signed_url = blob.generate_signed_url(
             expiration=time() + expire_time,
             response_type='text/plain',
-            method=method
+            method=method,
         )
         return signed_url
 
