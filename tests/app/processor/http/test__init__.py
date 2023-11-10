22e699d0ec9270fd3f44da5173d6ecdc1ba0f727
benson.wang <bensonwang2001@gmail.com>
2023-11-10 16:43:19 +0800

    chore: add test for register router

---
+++ tests/app/processor/http/test__init__.py
@@ -0,0 +1,10 @@
+from app.processor.http import register_routers
+from tests import Mock, TestCase
+
+
+class TestRegisterRouters(TestCase):
+    def test_happy_path(self):
+        app = Mock()
+        app.include_router = Mock()
+        register_routers(app=app)
+        self.assertEqual(app.include_router.call_count, 4)
