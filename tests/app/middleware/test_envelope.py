4fc42db06b6245da5d567a239cade8f7714c58ad
wsa <bensonwang2001@gmail.com>
2023-11-14 00:45:32 +0800

    chore: add test for envelope middleware

--- tests/app/middleware/test_envelope.py
+++ tests/app/middleware/test_envelope.py
@@ -1,6 +1,7 @@
 import logging
 
 from fastapi import Request
+from pydantic import BaseModel
 
 import app.exceptions as exc
 from app.config import app_config
@@ -8,10 +9,13 @@ from app.middleware.envelope import middleware
 from tests import AsyncMock, AsyncTestCase
 
 
+class ValidationErrorModel(BaseModel):
+    data: str
+
+
 class TestMiddleware(AsyncTestCase):
     def setUp(self) -> None:
         self.request = Request({'type': 'http', 'method': 'GET', 'headers': []})
-        logging.getLogger(app_config.logger_name).disabled = True
 
     async def test_happy_path(self):
         call_next = AsyncMock(return_value=None)
@@ -41,3 +45,12 @@ class TestMiddleware(AsyncTestCase):
         result = await middleware(self.request, call_next)
         self.assertEqual(result.status_code, 500)
         self.assertEqual(result.body, b'{"data":null,"error":"TypeError"}')
+
+    async def test_validation_error(self):
+        # Since can't raise validation error directly, make a validation error manually.
+        async def raise_validation_error(*args):
+            return ValidationErrorModel(data=1)  # noqa
+
+        result = await middleware(self.request, raise_validation_error)
+        self.assertEqual(result.status_code, 422)
+        self.assertEqual(result.body, b'{"data":null,"error":"IllegalInput"}')
