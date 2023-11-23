f3536afbaa5071b705fbc1d544d2c88184b8048c
wsa-2002 <91510760+wsa-2002@users.noreply.github.com>
2023-11-23 14:02:40 +0800

    feat: browse court (#14)

--- app/persistence/database/court.py
+++ app/persistence/database/court.py
@@ -1,3 +1,5 @@
+from typing import Sequence
+
 import app.exceptions as exc
 from app.base import do
 from app.persistence.database.util import PostgresQueryExecutor
@@ -15,3 +17,20 @@ async def read(court_id: int) -> do.Court:
         raise exc.NotFound
 
     return do.Court(id=id_, venue_id=venue_id)
+
+
+async def browse(venue_id: int) -> Sequence[do.Court]:
+    results = await PostgresQueryExecutor(
+        sql='SELECT id, venue_id'
+            '  FROM court'
+            ' WHERE venue_id = %(venue_id)s',
+        venue_id=venue_id, fetch='all',
+    ).fetch_all()
+
+    return [
+        do.Court(
+            id=id_,
+            venue_id=venue_id,
+        )
+        for id_, venue_id in results
+    ]
--- tests/app/persistence/database/test_court.py
+++ tests/app/persistence/database/test_court.py
@@ -41,3 +41,37 @@ class TestRead(AsyncTestCase):
                 ' WHERE id = %(court_id)s',
             court_id=self.court_id, fetch=1,
         )
+
+
+class TestBrowse(AsyncTestCase):
+    def setUp(self) -> None:
+        self.venue_id = 1
+        self.raw_court = [
+            (1, 1),
+            (2, 1),
+        ]
+        self.courts = [
+            do.Court(
+                id=1,
+                venue_id=1,
+            ),
+            do.Court(
+                id=2,
+                venue_id=1,
+            ),
+        ]
+
+    @patch('app.persistence.database.util.PostgresQueryExecutor.__init__', new_callable=Mock)
+    @patch('app.persistence.database.util.PostgresQueryExecutor.fetch_all', new_callable=AsyncMock)
+    async def test_happy_path(self, mock_fetch: AsyncMock, mock_init: Mock):
+        mock_fetch.return_value = self.raw_court
+
+        result = await court.browse(venue_id=self.venue_id)
+
+        self.assertEqual(result, self.courts)
+        mock_init.assert_called_with(
+            sql='SELECT id, venue_id'
+                '  FROM court'
+                ' WHERE venue_id = %(venue_id)s',
+            venue_id=self.venue_id, fetch='all',
+        )
