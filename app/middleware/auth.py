31f8cec9f201787ca647cc6b427c4aed2a13915c
wsa <bensonwang2001@gmail.com>
2023-11-25 08:52:51 +0800

    fix: access control allow origin

--- app/middleware/auth.py
+++ app/middleware/auth.py
@@ -4,6 +4,7 @@ import uuid
 from fastapi import Request
 
 from app.utils.context import context
+from app.config import app_config
 
 
 async def middleware(request: Request, call_next):
@@ -12,5 +13,5 @@ async def middleware(request: Request, call_next):
     context.set_request_uuid(request_uuid)
     response = await call_next(request)
     response.headers['X-Request-UUID'] = str(request_uuid)
-    response.headers['Access-Control-Allow-Origin'] = "*"
+    response.headers['Access-Control-Allow-Origin'] = ', '.join(app_config.allow_origins)
     return response
