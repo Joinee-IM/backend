73a60ec26ed347eac5b1688f3754bd0c1e3f4ece
wsa <bensonwang2001@gmail.com>
2023-11-27 01:53:08 +0800

    fix: remove http only

--- app/processor/http/google.py
+++ app/processor/http/google.py
@@ -52,8 +52,8 @@ async def auth(request: Request):
             )
         token = encode_jwt(account_id=account_id)
         response = RedirectResponse(url=f"{service_config.url}/", status_code=302)
-        response.set_cookie(key="account_id", value=str(account_id), httponly=True, samesite='lax', secure=True)
-        response.set_cookie(key="token", value=str(token), httponly=True, samesite='lax', secure=True)
+        response.set_cookie(key="account_id", value=str(account_id), samesite='none', secure=True)
+        response.set_cookie(key="token", value=str(token), samesite='none', secure=True)
         return response
 
 
