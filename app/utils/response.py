ecd51e4ae0777ef82772263b8646e383e0830599
wsa-2002 <91510760+wsa-2002@users.noreply.github.com>
2023-12-02 10:04:25 +0800

    feat: add exception response into swagger doc (#47)

--- app/utils/response.py
+++ app/utils/response.py
@@ -1,3 +1,5 @@
+import ast
+import enum
 import typing
 
 import pydantic
@@ -5,6 +7,23 @@ import pydantic
 T = typing.TypeVar('T')
 
 
+class ClassNameExtractor(ast.NodeVisitor):
+    def __init__(self, file_path: str):
+        self.class_names = []
+        with open(file_path, 'r') as file:
+            content = file.read()
+            tree = ast.parse(content)
+        self.visit(tree)
+
+    def visit_ClassDef(self, node):
+        self.class_names.append(node.name)
+        self.generic_visit(node)
+
+
+class_names: typing.List[str] = ClassNameExtractor('app/exceptions/__init__.py').class_names
+class_enum = enum.Enum('ErrorMessage', {class_name: class_name for class_name in class_names})
+
+
 class Response(pydantic.BaseModel, typing.Generic[T]):
     data: T | None = None
-    error: str | None = None
+    error: class_enum | None = None
