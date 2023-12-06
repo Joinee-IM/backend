e2e75b91f8efc8263ff1b73e30de32c2a1956faf
wsa <bensonwang2001@gmail.com>
2023-12-06 18:34:51 +0800

    feat: return exception while google login's account not created

--- app/processor/http/google.py
+++ app/processor/http/google.py
@@ -43,14 +43,18 @@ async def auth(request: Request):
                 refresh_token=token_google['refresh_token'],
             )
         except exc.NotFound:
-            role = enums.RoleType(request.query_params.get('state'))
-            account_id = await db.account.add(
-                email=user_email, is_google_login=True,
-                nickname=user_email.split("@")[0],
-                role=role,
-                access_token=token_google['access_token'],
-                refresh_token=token_google['refresh_token'],
-            )
+            try:
+                role = enums.RoleType(request.query_params.get('state'))
+                account_id = await db.account.add(
+                    email=user_email, is_google_login=True,
+                    nickname=user_email.split("@")[0],
+                    role=role,
+                    access_token=token_google['access_token'],
+                    refresh_token=token_google['refresh_token'],
+                )
+            except ValueError:  # no role & no account
+                return RedirectResponse(url=f"{service_config.url}?error=LoginFailed", status_code=303)
+
         token = encode_jwt(account_id=account_id, role=role)
         response = RedirectResponse(url=f"{service_config.url}", status_code=303)
         response.set_cookie(key="account_id", value=str(account_id), samesite='none', secure=True, httponly=True)
