5a4da28f855f7feb3aa21637c44963d093719113
Benson Wang <91510760+wsa-2002@users.noreply.github.com>
2023-12-08 22:11:29 +0800

    fix: return all sport name and business hour in browse stadium result (#73)

--- app/persistence/database/stadium.py
+++ app/persistence/database/stadium.py
@@ -60,22 +60,33 @@ async def browse(
         fr'       description, long, lat, stadium.is_published,'
         fr'       city.name,'
         fr'       district.name,'
-        fr'       ARRAY_AGG(DISTINCT sport.name),'
-        fr'       ARRAY_AGG(DISTINCT business_hour.*)'
+        fr'       ARRAY_AGG(DISTINCT sport.name) AS sport_names,'
+        fr'       ARRAY_AGG(DISTINCT business_hour.*) AS business_hour'
         fr'  FROM stadium'
+        fr' INNER JOIN ('
+        fr'     SELECT stadium.id AS stadium_id'
+        fr'       FROM stadium'
+        fr'      INNER JOIN district ON stadium.district_id = district.id'
+        fr'      INNER JOIN city ON district.city_id = city.id'
+        fr'       LEFT JOIN venue ON stadium.id = venue.stadium_id'
+        fr'       LEFT JOIN sport ON venue.sport_id = sport.id'
+        fr'       LEFT JOIN business_hour ON business_hour.place_id = stadium.id'
+        fr'             AND business_hour.type = %(place_type)s'
+        fr' {where_sql}'
+        fr'        GROUP BY stadium.id, city.id, district.id'
+        fr'   ) tbl ON tbl.stadium_id = stadium.id'
         fr' INNER JOIN district ON stadium.district_id = district.id'
         fr' INNER JOIN city ON district.city_id = city.id'
         fr'  LEFT JOIN venue ON stadium.id = venue.stadium_id'
         fr'  LEFT JOIN sport ON venue.sport_id = sport.id'
         fr'  LEFT JOIN business_hour ON business_hour.place_id = stadium.id'
         fr'                         AND business_hour.type = %(place_type)s'
-        fr' {where_sql}'
         fr' GROUP BY stadium.id, city.id, district.id'
-        fr' ORDER BY stadium.id'
     )
 
     results = await PostgresQueryExecutor(
         sql=fr'{sql}'
+            fr' ORDER BY stadium.id'
             fr' LIMIT %(limit)s OFFSET %(offset)s',
         limit=limit, offset=offset, place_type=enums.PlaceType.stadium, **params,
     ).fetch_all()
