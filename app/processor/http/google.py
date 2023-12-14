869a7d5bd074389cc1b46838702d2b9ec47705e3
wsa <bensonwang2001@gmail.com>
2023-12-14 16:00:14 +0800

    fix: add role in redirect url

--- app/processor/http/google.py
+++ app/processor/http/google.py
@@ -64,7 +64,7 @@ async def auth(request: Request):
         )
 
     token = encode_jwt(account_id=account_id, role=role)
-    response = RedirectResponse(url=f'{next_url}?{account_id=}&{error=}', status_code=302)
+    response = RedirectResponse(url=f'{next_url}?{account_id=}?{role=}&{error=}', status_code=302)
     response = update_cookie(response=response, account_id=account_id, token=token, role=role)
     return response
 
